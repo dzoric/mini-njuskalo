964d044843c799da60cf28f0dead594172eb7fda
dzoric <zoricdusan33@gmail.com>
2019-03-13 11:35:48 +0100

    Add project files.

---
+++ Newskalo.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Newskalo.MVC.MvcApplication" Language="C#" %>
---
+++ Newskalo.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Newskalo.MVC.Startup))]
+namespace Newskalo.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
